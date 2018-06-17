c3b4a7b6c0ffea01dbc1c7cbd196528337e8f354
Shadowsong27 <syk950527@gmail.com>
2018-06-17 20:19:38 +0800

    adding swift tutorial

---
+++ swift-tutorial/swift-tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-tutorial
+//
+//  Created by Song Yikun on 17/6/18.
+//  Copyright © 2018 Shadowsong's Hub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
